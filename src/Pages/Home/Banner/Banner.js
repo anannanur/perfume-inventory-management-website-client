034c7d003ed0ee612d4b4084fe52a75a2aedf400
anannanur <anannaanu974@gmail.com>
2022-05-05 19:51:33 +0600

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="animation-area">
+            <div className="banner-title">
+                <h1>Welcome to <span>Perfume</span> Inventory System..</h1>
+            </div>
+            <ul class="box-area">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Banner;
