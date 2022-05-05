4978c596edea72de21a96e109281558a3ffd14c6
anannanur <anannaanu974@gmail.com>
2022-05-05 23:43:46 +0600

    statistics section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Items from '../Items/Items';
+import Statistics from '../Statistics/Statistics';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            
             <Items></Items>
+            <Statistics></Statistics>
         </div>
     );
 };
---
+++ src/Pages/Home/Statistics/Statistics.js
@@ -0,0 +1,37 @@
+import { faAngleDoubleRight, faArrowPointer, faSortAmountUp, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Statistics.css';
+import CountUp from 'react-countup';
+
+const Statistics = () => {
+    return (
+        <div className='statistics-section bg-dark py-5 my-5'>
+            <h1 className='text-center text-warning fw-bold py-5'>Some Statistics</h1>
+            <div className="row text-warning pt-4 pb-5 mt-5">
+                <div className="col-12 col-md-3 text-center">
+               <h1> <FontAwesomeIcon icon={faAngleDoubleRight} /></h1>
+                    <h1><CountUp end={2000} duration={5}/></h1>
+                    <p>Perfume Sold</p>
+                </div>
+                <div className="col-12 col-md-3 text-center">
+                <h1><FontAwesomeIcon icon={faSortAmountUp} /></h1>
+                    <h1>৳<CountUp end={100000} duration={5}/></h1>
+                    <p>Amount Sold</p>
+                </div>
+                <div className="col-12 col-md-3 text-center">
+                <h1><FontAwesomeIcon icon={faStar} /></h1>
+                    <h1><CountUp end={100} duration={5}/>%</h1>
+                    <p>Customer Satisfaction</p>
+                </div>
+                <div className="col-12 col-md-3 text-center">
+                <h1><FontAwesomeIcon icon={faArrowPointer} /></h1>
+                    <h1><CountUp end={3600} duration={5}/></h1>
+                    <p>Product changes</p>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Statistics;
