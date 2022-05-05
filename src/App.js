096727fb746eefc021ed584260ed7fc06315211b
anannanur <anannaanu974@gmail.com>
2022-05-05 18:31:25 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import  Header  from './Pages/Shared/Header/Header'
 import  Footer from './Pages/Shared/Footer/Footer';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar, } from 'react-bootstrap';
+import './Header.css';
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+    return (       
+            <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Perfume</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link className="fs-5 me-1" href="#features">Home</Nav.Link>
+                            <Nav.Link className="fs-5 me-1" href="#pricing">Manage Inventory</Nav.Link>
+                            <Nav.Link className="fs-5 me-1" href="#pricing">My Item</Nav.Link>
+                            <Nav.Link className="fs-5 me-1" href="#pricing">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
     );
 };
 
