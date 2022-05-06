83f10d21a161fc05b3b5a42125331b0895009dab
anannanur <anannaanu974@gmail.com>
2022-05-06 11:43:20 +0600

    env-local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDe6sfDuPcfQxnBVaXjssAn2uP1GBiStvo",
-    authDomain: "perfume-inventory-system.firebaseapp.com",
-    projectId: "perfume-inventory-system",
-    storageBucket: "perfume-inventory-system.appspot.com",
-    messagingSenderId: "293385595737",
-    appId: "1:293385595737:web:833188d16b06169b0b3a48"
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket:process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
