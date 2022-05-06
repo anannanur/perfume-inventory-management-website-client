9be57a57f4ad4a5dd62115b04c8d553b04fb8f71
anannanur <anannaanu974@gmail.com>
2022-05-06 11:20:25 +0600

    registration form added

---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,55 @@
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import img from '../../../images/register.jpg';
+
+const Register = () => {
+    const navigate = useNavigate();
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const userNameRef = useRef('');
+
+    const navigateLogin = event => {
+        navigate(`/login`);
+    }
+    return (
+        <div className='login py-5 bg-dark'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6 text-center px-5">
+                        <img className="w-100 img-fluid" src={img} alt="" />
+                    </div>
+                    <div className="col-12 col-md-6 px-5">
+                        <h1 className='text-center pt-3 fw-bold text-info'>Please Register</h1>
+                        <form>
+                            <div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputtext" className="form-label text-muted">Your Name</label>
+                                    <input ref={userNameRef} type="text" className="form-control" id="exampleInputtext1" required />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label text-muted">Email address</label>
+                                    <input ref={emailRef} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputPassword1" className="form-label text-muted">Password</label>
+                                    <input ref={passwordRef} type="password" className="form-control" id="exampleInputPassword1" required />
+                                </div>
+                                <div className='my-4 d-grid'>
+                                    <button type="submit" className="btn rounded-pill text-white fw-bold bg-info">Register</button>
+                                </div>
+                            </div>
+                            <p className='mt-2 text-muted'>Already registered? <Link onClick={navigateLogin} to='/login' className='text-decoration-none pe-auto text-info'>Please Login</Link></p>
+                            {/* <div className='d-flex'>
+                                <p className='mt-2'>Forgot Password?</p>
+                                <button  className='btn btn-link pt-0 mt-0 text-decoration-none' style={{ color: '#ea8685' }}>Please Reset</button>
+                            </div> */}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
