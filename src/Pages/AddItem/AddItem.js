58d24a580209a3b26b5234c1d8eda7d3a2e3c340
anannanur <anannaanu974@gmail.com>
2022-05-07 11:56:51 +0600

    toast added in additem component and reset input functionality is also done

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import img from '../../images/additem.jpg';
 import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
+    const [val, setVal] = useState();
     const onSubmit = data => {
         console.log(data);
         const url = `http://localhost:5000/perfume`;
@@ -18,7 +21,13 @@ const AddItem = () => {
             .then(result => {
                 console.log(result);
             })
+
     };
+    const notify = () => {
+        toast('New item added');
+        setVal('');
+    };
+
     return (
         <div className='bg-dark pb-4'>
             <h2 className='text-info text-center pt-2 pb-4 fw-bold'>Add New Item</h2>
@@ -26,19 +35,22 @@ const AddItem = () => {
                 <div className="row d-flex">
                     <div className="col-12 col-md-6 mt-3 order-2 order-md-1 mt-2">
                         <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
-                            <input style={{ border: 'none' }} className='mb-3 rounded p-2 bg-light' placeholder='Enter name' {...register("name", { required: true })} />
-                            <input style={{ border: 'none' }} className='mb-3 rounded p-2 bg-light' placeholder='Enter photo url' {...register("img")} />
-                            <textarea style={{ border: 'none' }} className='mb-3 rounded p-2 bg-light' placeholder='Enter description' {...register("description")} />
-                            <input style={{ border: 'none' }} className='mb-3 rounded p-2 bg-light' placeholder='Enter price' type="number" {...register("price")} />
-                            <input style={{ border: 'none' }} className='mb-3 rounded p-2 bg-light' placeholder='Enter quantity' type="number" {...register("quantity")} />
-                            <input style={{ border: 'none' }} className='mb-3 rounded p-2 bg-light' placeholder='Enter supplier' {...register("supplier")} />
-                            <input className="btn btn-outline-info fw-bold text-white" type="submit" value="Add Item" />
+                            <input style={{ border: 'none' }} value={val} className='mb-3 rounded p-2 bg-light' placeholder='Enter name' {...register("name", { required: true })} />
+                            <input style={{ border: 'none' }} value={val} className='mb-3 rounded p-2 bg-light' placeholder='Enter photo url' {...register("img")} />
+                            <textarea style={{ border: 'none' }} value={val} className='mb-3 rounded p-2 bg-light' placeholder='Enter description' {...register("description")} />
+                            <input style={{ border: 'none' }} value={val} className='mb-3 rounded p-2 bg-light' placeholder='Enter price' type="number" {...register("price")} />
+                            <input style={{ border: 'none' }} value={val} className='mb-3 rounded p-2 bg-light' placeholder='Enter quantity' type="number" {...register("quantity")} />
+                            <input style={{ border: 'none' }} value={val} className='mb-3 rounded p-2 bg-light' placeholder='Enter supplier' {...register("supplier")} />
+
+                            <div className='text-center mt-2'>
+                                <input onClick={notify} className="w-50 btn btn-outline-info fw-bold text-white" value='Add item' type="submit" />
+                            </div>
                         </form>
                     </div>
                     <div className="col-12 col-md-6 text-center order-1 order-md-2">
                         <img src={img} alt="" className='img-fluid ' />
                     </div>
-
+                    <ToastContainer />
                 </div>
             </div>
         </div>
